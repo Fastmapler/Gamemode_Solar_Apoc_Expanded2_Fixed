3286a5eaf55924464dfd7af3755acfea1086d5d9
Fastmapler <Fastmapler@Fastmapler.com>
2023-05-28 00:07:03 -0400

    bruh

--- Modules/Tools/Tool_InvExpander.cs
+++ Modules/Tools/Tool_InvExpander.cs
@@ -209,7 +209,7 @@ datablock ShapeBaseImageData(BatteryExpanderImage : InvExpanderImage)
 {
    shapeFile = "./Shapes/Battery.dts";
    item = BatteryExpanderItem;
-   BatteryExpanderImage.printPlayerBattery = true;
+   printPlayerBattery = true;
 };
 
 function getBatteryUpgradeCost(%amount)
