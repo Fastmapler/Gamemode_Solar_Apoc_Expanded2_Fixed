624dc790c3d805ce7a8e3852f962f5e8e78073ca
Fastmapler <=>
2025-02-16 23:33:26 -0500

    Remove Event_Variables for gamemode dependency

--- Modules/AddOns/AddOns.cs
+++ Modules/AddOns/AddOns.cs
@@ -2,6 +2,5 @@ exec("./Event_Storage/server.cs");
 exec("./Support_Autosaver/server.cs");
 exec("./Support_ShapelinesV2/server.cs");
 exec("./Bot_Hole/server.cs");
-exec("./Event_Variables/server.cs");
 exec("./Script_PeggFootsteps/server.cs");
 //exec("./Support_DataInstance/dataInstance.cs");
--- Modules/Core/Core.cs
+++ Modules/Core/Core.cs
@@ -64,6 +64,159 @@ function createDefaultMinigame()
 }
 schedule(10, 0, "createDefaultMinigame");
 
+function setVCEVariables()
+{
+    unregisterSpecialVar(GameConnection,"kdratio");
+    
+    unregisterSpecialVar(GameConnection,"clanPrefix");
+	registerSpecialVar(GameConnection,"clanPrefix","%this.clanPrefix");
+
+    unregisterSpecialVar(GameConnection,"clanSuffix");
+	registerSpecialVar(GameConnection,"clanSuffix","%this.clanSuffix");
+
+    unregisterSpecialVar(GameConnection,"score");
+    registerSpecialVar(GameConnection,"score","%this.score");
+
+    unregisterSpecialVar(Player,"energy");
+    registerSpecialVar(Player,"energy","%this.getEnergyLevel()");
+
+    unregisterSpecialVar(Player,"damage");
+	registerSpecialVar(Player,"damage","%this.getDamageLevel()");
+
+    unregisterSpecialVar(Player,"health");
+	registerSpecialVar(Player,"health","%this.getDatablock().maxDamage - %this.getDamageLevel()");
+
+    unregisterSpecialVar(Player,"vel");
+    registerSpecialVar(Player,"vel","getWords(%this.getVelocity(),0,2)");
+
+    unregisterSpecialVar(Player,"currentitem","%this.tool[%this.currTool].uiName","setCurrentItem");
+    registerSpecialVar(Player,"currentitem","%this.tool[%this.currTool].uiName"); 
+    
+	unregisterSpecialVar(Player,"item1");
+    registerSpecialVar(Player,"item1","%this.tool[0].uiName");
+
+	unregisterSpecialVar(Player,"item2");
+    registerSpecialVar(Player,"item2","%this.tool[1].uiName");
+
+	unregisterSpecialVar(Player,"item3");
+    registerSpecialVar(Player,"item3","%this.tool[2].uiName");
+
+	unregisterSpecialVar(Player,"item4");
+    registerSpecialVar(Player,"item4","%this.tool[3].uiName");
+
+	unregisterSpecialVar(Player,"item5");
+    registerSpecialVar(Player,"item5","%this.tool[4].uiName");
+
+	unregisterSpecialVar(Player,"item6");
+    registerSpecialVar(Player,"item6","%this.tool[5].uiName");
+
+	unregisterSpecialVar(Player,"item7");
+    registerSpecialVar(Player,"item7","%this.tool[6].uiName");
+
+	unregisterSpecialVar(Player,"item8");
+    registerSpecialVar(Player,"item8","%this.tool[7].uiName");
+
+	unregisterSpecialVar(Player,"item9");
+    registerSpecialVar(Player,"item9","%this.tool[8].uiName");
+
+	unregisterSpecialVar(Player,"item10");
+    registerSpecialVar(Player,"item10","%this.tool[9].uiName");
+
+    unregisterSpecialVar(Vehicle,"damage");
+    registerSpecialVar(Vehicle,"damage","%this.getDamageLevel()");
+
+    unregisterSpecialVar(Vehicle,"health");
+	registerSpecialVar(Vehicle,"health","%this.getDatablock().maxDamage - %this.getDamageLevel()");
+
+    unregisterSpecialVar(Vehicle,"vel");
+    registerSpecialVar(Vehicle,"vel","%this.getVelocity()");
+
+    unregisterSpecialVar(Vehicle,"pos");
+    registerSpecialVar(Vehicle,"pos","getWords(%this.getTransform(),0,2)");
+
+    //Remove bot variables
+    unregisterSpecialVar(Bot,"yaw");
+    unregisterSpecialVar(Bot,"pitch");
+    unregisterSpecialVar(Bot,"hat");
+    unregisterSpecialVar(Bot,"accent");
+    unregisterSpecialVar(Bot,"pack");
+    unregisterSpecialVar(Bot,"secondPack");
+    unregisterSpecialVar(Bot,"hip");
+    unregisterSpecialVar(Bot,"rleg");
+    unregisterSpecialVar(Bot,"rarm");
+    unregisterSpecialVar(Bot,"lleg");
+    unregisterSpecialVar(Bot,"larm");
+    unregisterSpecialVar(Bot,"chest");
+    unregisterSpecialVar(Bot,"decal");
+    unregisterSpecialVar(Bot,"face");
+    unregisterSpecialVar(Bot,"energy");
+    unregisterSpecialVar(Bot,"damage");
+    unregisterSpecialVar(Bot,"health");
+    unregisterSpecialVar(Bot,"velx");
+    unregisterSpecialVar(Bot,"vely");
+    unregisterSpecialVar(Bot,"velz");
+    unregisterSpecialVar(Bot,"vel");
+    unregisterSpecialVar(Bot,"posx");
+    unregisterSpecialVar(Bot,"posy");
+    unregisterSpecialVar(Bot,"posz");
+    unregisterSpecialVar(Bot,"pos");
+    unregisterSpecialVar(Bot,"speed");
+    unregisterSpecialVar(Bot,"crouching");
+    unregisterSpecialVar(Bot,"jumping");
+    unregisterSpecialVar(Bot,"jetting");
+    unregisterSpecialVar(Bot,"firing");
+    unregisterSpecialVar(Bot,"sitting");
+    unregisterSpecialVar(Bot,"datablock");
+    unregisterSpecialVar(Bot,"weapon");
+    unregisterSpecialVar(Bot,"type");
+    unregisterSpecialVar(Bot,"state");
+    unregisterSpecialVar(Bot,"enabled");
+
+    //Solar Apoc special stuff
+    registerSpecialVar(fxDTSbrick,"brickPower","%this.getPower()");
+    registerSpecialVar(fxDTSbrick,"netPower","%this.cablenet.powerBuffer");
+    registerSpecialVar(fxDTSbrick,"machineDisabled","%this.machineDisabled");
+    registerSpecialVar(fxDTSbrick,"processingRecipe","cleanRecipeName(%this.processingRecipe)");
+    registerSpecialVar(fxDTSbrick,"recipeProgress","%this.recipeProgress");
+    
+    registerSpecialVar(fxDTSbrick,"input1name","getField(%this.matter[Input, 0], 0)");
+    registerSpecialVar(fxDTSbrick,"input1amount","getField(%this.matter[Input, 0], 1)");
+    registerSpecialVar(fxDTSbrick,"input2name","getField(%this.matter[Input, 1], 0)");
+    registerSpecialVar(fxDTSbrick,"input2amount","getField(%this.matter[Input, 1], 1)");
+    registerSpecialVar(fxDTSbrick,"input3name","getField(%this.matter[Input, 2], 0)");
+    registerSpecialVar(fxDTSbrick,"input3amount","getField(%this.matter[Input, 2], 1)");
+    registerSpecialVar(fxDTSbrick,"input4name","getField(%this.matter[Input, 3], 0)");
+    registerSpecialVar(fxDTSbrick,"input4amount","getField(%this.matter[Input, 3], 1)");
+
+    registerSpecialVar(fxDTSbrick,"buffer1name","getField(%this.matter[buffer, 0], 0)");
+    registerSpecialVar(fxDTSbrick,"buffer1amount","getField(%this.matter[buffer, 0], 1)");
+    registerSpecialVar(fxDTSbrick,"buffer2name","getField(%this.matter[buffer, 1], 0)");
+    registerSpecialVar(fxDTSbrick,"buffer2amount","getField(%this.matter[buffer, 1], 1)");
+    registerSpecialVar(fxDTSbrick,"buffer3name","getField(%this.matter[buffer, 2], 0)");
+    registerSpecialVar(fxDTSbrick,"buffer3amount","getField(%this.matter[buffer, 2], 1)");
+    registerSpecialVar(fxDTSbrick,"buffer4name","getField(%this.matter[buffer, 3], 0)");
+    registerSpecialVar(fxDTSbrick,"buffer4amount","getField(%this.matter[buffer, 3], 1)");
+
+    registerSpecialVar(fxDTSbrick,"output1name","getField(%this.matter[output, 0], 0)");
+    registerSpecialVar(fxDTSbrick,"output1amount","getField(%this.matter[output, 0], 1)");
+    registerSpecialVar(fxDTSbrick,"output2name","getField(%this.matter[output, 1], 0)");
+    registerSpecialVar(fxDTSbrick,"output2amount","getField(%this.matter[output, 1], 1)");
+    registerSpecialVar(fxDTSbrick,"output3name","getField(%this.matter[output, 2], 0)");
+    registerSpecialVar(fxDTSbrick,"output3amount","getField(%this.matter[output, 2], 1)");
+    registerSpecialVar(fxDTSbrick,"output4name","getField(%this.matter[output, 3], 0)");
+    registerSpecialVar(fxDTSbrick,"output4amount","getField(%this.matter[output, 3], 1)");
+
+    registerSpecialVar(fxDTSbrick,"ugVeinSize","%this.getUGVeinComp()");
+    registerSpecialVar(fxDTSbrick,"ugVeinType","%this.getUGVeinType()");
+
+    registerSpecialVar(GameConnection,"scanMaterialName","%this.scanMaterialName");
+    registerSpecialVar(GameConnection,"scanMaterialAmount","$EOTW::Material[%this.bl_id, %this.scanMaterialName]");
+
+    registerSpecialVar("GLOBAL","SA_Day","$EOTW::Day");
+    registerSpecialVar("GLOBAL","SA_Time","$EOTW::Time");
+}
+setVCEVariables();
+
 package NoEarlyJoin
 {
 	function servAuthTCPobj::onLine(%this, %line)
